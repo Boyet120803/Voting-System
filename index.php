505b6a4f33b6f0ce170f6fd46db71e31ba2459da
boyet_dedal <dedalboyet@gmail.com>
2024-06-16 18:59:16 +0800

    for index

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['ID']) && isset($_SESSION['email'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
    
     <link rel="stylesheet" href="homepage.css">
 </head>
